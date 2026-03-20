af56943aca34eceeb801911cf5272d43ea908b78
astra-void <dev@astra-void.xyz>
2026-03-20 11:41:44 +0900

    style(radio-group): reorder index exports

--- packages/radio-group/src/index.ts
+++ packages/radio-group/src/index.ts
@@ -12,15 +12,15 @@ export const RadioGroup = {
   Indicator: typeof RadioGroupIndicator;
 };
 
-export { RadioGroupIndicator, RadioGroupItem, RadioGroupRoot };
 
 export type {
   RadioGroupContextValue,
   RadioGroupIndicatorProps,
   RadioGroupItemContextValue,
-  RadioGroupItemRegistration,
   RadioGroupItemProps,
+  RadioGroupItemRegistration,
   RadioGroupOrientation,
   RadioGroupProps,
   RadioGroupSetValue,
 } from "./RadioGroup/types";
+export { RadioGroupIndicator, RadioGroupItem, RadioGroupRoot };
